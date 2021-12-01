fe531ddcdcc76de65781f51a878309af3968d0c8
Alberto Díez <Alberto.Diez@bosonit.com>
2021-12-01 10:32:33 +0100

    Actualización para meter rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule, enableDebugTools } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
@@ -15,7 +15,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { PersonaDetalleComponent } from './persona-detalle/persona-detalle.component';
 
-
+ 
 
 
 import { MatDialogModule } from '@angular/material/dialog';
@@ -23,10 +23,36 @@ import { ReactivoDetallePersonaComponent } from './reactivo-detalle-persona/reac
 import { ReactiveFormsModule } from '@angular/forms';
 import { MessagesComponent } from './messages/messages.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component'
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
+import { MenuComponent } from './menu/menu.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'AppComponent'
+  },
+  {
+    path: 'visualizacion_tabla',
+    component: PersonasOutputComponent,
+  },
+  {
+    path: 'visualizacion_rectangulos',
+    component: DashboardComponent,
+  }
+
+
+];
 
  
+
 @NgModule({
+  exports: [
+    RouterModule
+    ],
   declarations: [
     AppComponent,
     PersonasComponent,
@@ -34,9 +60,19 @@ import { MessagesComponent } from './messages/messages.component';
     PersonasOutputComponent,
     PersonaDetalleComponent,
     ReactivoDetallePersonaComponent,
-    MessagesComponent
+    MessagesComponent,
+    BienvenidoComponent,
+    MenuComponent
   ],
   imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes, {
+      enableTracing: true, // for debug the routes
+      paramsInheritanceStrategy: 'always', // heredar los datos de la ruta padre
+      useHash: true // for server configuration routing
+    }),
     HttpClientModule,
     MatSliderModule,
     BrowserModule,
@@ -54,6 +90,7 @@ import { MessagesComponent } from './messages/messages.component';
     FormsModule,
     MatTableModule,
     ReactiveFormsModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
